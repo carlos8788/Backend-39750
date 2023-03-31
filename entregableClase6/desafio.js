ea571d5310b5d3baf2ea4d9fe6902f13bc1135e8
carlos8788 <carlos8788@gmail.com>
2023-03-31 14:46:40 -0300

    entregableClase6

---
+++ entregableClase6/desafio.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import ProductManager from './manejoDeArchivos.js';
+// const pm = require('manejoDeArchivos');
+
+const pm = new ProductManager();
+const app = express();
+
+
+
+app.get('/products/', (req, res) => {
+
+    const { limit } = req.query
+    pm.getProducts().then(products => {
+        const limitProduts = products.slice(0, limit)
+        res.send(limitProduts)})
+        .catch(err => res.status(500).send(err))
+})
+
+app.get('/products/:pid', (req, res) => {
+    console.log(req.params.pid);
+
+    pm.getProductById(req.params.pid)
+        .then(product =>{
+            
+            (res.send(product))
+        })
+        .catch(err => res.status(500).send(err))
+})
+
+app.listen(8080, () => {
+    console.log('Estoy escuchando el puerto 8080');
+});
